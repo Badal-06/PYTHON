990f0db6793c5f1a989628dadffc038c49f71a2d
BADAL <badalpanwar91@gmail.com>
2025-05-04 13:19:06 +0530

    Add files via upload

---
+++ BASICS_PYTHON_CODES/DATA_TYPES_IN_PYTHON.py
@@ -0,0 +1,10 @@
+name ="badal" 
+age = 20 
+price = 90.5 
+marks = None 
+boy = True
+print(type(name)) # string
+print(type(age)) # integer
+print(type(price)) # float
+print (type(marks)) # NoneType
+print (type(boy)) # boolean
