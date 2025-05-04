967f536f093b627c1b11640a74ac9ce02183f5c7
badal-06 <badalpanwar91@gmail.com>
2025-05-04 16:29:54 +0530

    Code for Grades of Students Using If Else

---
+++ Python_Mini_Projects/Grades_of_Students.py
@@ -0,0 +1,12 @@
+
+# code for grades of student: -
+
+marks=int(input("marks:"))
+if(marks>=90):
+    print("A")
+elif(marks>=80):
+    print("B")
+elif(marks>=70):
+    print("C")
+else:
+    print("D")  
