e7476f64a12b04123edebd9a81765dbb42594658
badal-06 <badalpanwar91@gmail.com>
2025-05-07 23:26:50 +0530

    Operation on List in Python

---
+++ BASICS_PYTHON_CODES/List_OPerations.py
@@ -0,0 +1,11 @@
+list=[2,1,3]
+
+# Operation on List :-
+list.append(5) # [2,1,3,5]
+list.sort() # [1,2,3]
+list.sort(reverse=True) # [3,2,1]
+list.reverse() # [3,1,2]
+list.insert(2,6) # [1,2,6,3,5]
+list.remove(2) # [1,3]
+list.pop(2) # [2,1]
+print(list)
