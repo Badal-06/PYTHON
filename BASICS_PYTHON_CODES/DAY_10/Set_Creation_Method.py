28b774bce899e06e725639ab6170566068509d6d
badal-06 <badalpanwar91@gmail.com>
2025-08-10 22:49:18 +0530

    Add Set

---
+++ BASICS_PYTHON_CODES/DAY_10/Set_Creation_Method.py
@@ -0,0 +1,13 @@
+coll=set()
+coll.add("badal")
+coll.add(90)
+coll.remove(90)
+coll.clear()
+coll.add(9)
+print(coll)
+print(coll.pop())
+print(type(coll))
+set1= {1,2,3,4}
+set2={3,4,5,6}
+print(set1.union(set2))
+print(set1.intersection(set2))
