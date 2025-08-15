44ea2dcb58a6f46453d0cb8b1cc498e9f8ba9517
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:19:08 +0530

    Add comments to  no finding using while loop

--- BASICS_PYTHON_CODES/WHILE_LOOP/No_Finding_Using_While_Loop.py
+++ BASICS_PYTHON_CODES/WHILE_LOOP/No_Finding_Using_While_Loop.py
@@ -1,10 +1,20 @@
-nums =(1,4,9,16,25,36,49,64,81,100)
-x=49
-i=0
-while i<len(nums):
-    if(nums[i]==x):
-        print("found at idx",i)
-        break
+# Create a tuple of numbers
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+# The number we want to search for
+x = 49
+
+# Initialize index counter
+i = 0
+
+# Loop until the end of the tuple
+while i < len(nums):
+    # Check if the current element matches x
+    if nums[i] == x:
+        print("found at idx", i)  # Print the index where number is found
+        break                     # Exit the loop once found
     else:
-        print("finding")
-    i+=1
+        print("finding")           # Print message if not found yet
+    
+    # Move to the next index
+    i += 1
