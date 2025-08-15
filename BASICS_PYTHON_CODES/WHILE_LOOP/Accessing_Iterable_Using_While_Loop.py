a8dce6ef3490c84afe71cc434b472e08c0ce0da4
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:15:21 +0530

    Add comments to accessing iterable using while loop

--- BASICS_PYTHON_CODES/WHILE_LOOP/Accessing_Iterable_Using_While_Loop.py
+++ BASICS_PYTHON_CODES/WHILE_LOOP/Accessing_Iterable_Using_While_Loop.py
@@ -1,5 +1,13 @@
-nums =[1,4,9,16,25,36,49,64,81,100]
-idx =0
-while idx<len(nums):
+# List of numbers
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Starting index for iteration
+idx = 0
+
+# Loop until index reaches the length of the list
+while idx < len(nums):
+    # Print the element at the current index
     print(nums[idx])
+    
+    # Move to the next index
     idx += 1
