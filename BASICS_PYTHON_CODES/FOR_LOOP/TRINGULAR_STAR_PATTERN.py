f3e1b8db643a6c6e2ec5b66061454ac87e3d7be0
badal-06 <badalpanwar91@gmail.com>
2025-08-15 14:58:59 +0530

    add comments to tringular star pattren

--- BASICS_PYTHON_CODES/FOR_LOOP/TRINGULAR_STAR_PATTERN.py
+++ BASICS_PYTHON_CODES/FOR_LOOP/TRINGULAR_STAR_PATTERN.py
@@ -1,5 +1,16 @@
-n=int(input("enter a number: "))
-for i in range (1,n+1):
-    print(" "*(n-i),end="")
-    print("*"*(2*i-1),end="")
+# Take an integer input from the user (height of the pyramid)
+n = int(input("Enter a number: "))
+
+# Loop through each row from 1 to n
+for i in range(1, n + 1):
+
+    # Print spaces before the stars (for pyramid alignment)
+    # 'n - i' spaces ensure that the stars are centered
+    print(" " * (n - i), end="")
+
+    # Print stars: for row i, there are (2*i - 1) stars
+    # This creates an odd number of stars for each row (1, 3, 5, 7, ...)
+    print("*" * (2 * i - 1), end="")
+
+    # Move to the next line after printing spaces and stars
     print("")
