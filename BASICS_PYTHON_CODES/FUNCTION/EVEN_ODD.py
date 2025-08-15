3a161a0717ba7f3bce8ffa574f795ed76ef14268
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:05:45 +0530

    Add comments to even odd

--- BASICS_PYTHON_CODES/FUNCTION/EVEN_ODD.py
+++ BASICS_PYTHON_CODES/FUNCTION/EVEN_ODD.py
@@ -1,7 +1,14 @@
-#even_odd_function-
-num = int(input("enter your number:"))
-rem = num%2
-if(rem==0):
-   print("even")
+# Program to check if a number is even or odd
+
+# Take an integer input from the user
+num = int(input("Enter your number: "))
+
+# Find the remainder when the number is divided by 2
+rem = num % 2
+
+# If remainder is 0, the number is even
+if rem == 0:
+    print("even")
+# Otherwise, the number is odd
 else:
-   print("odd")
+    print("odd")
