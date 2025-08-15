487d8ae8e967690c63d263249f9f38500239e695
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:09:41 +0530

    Add comments to check odd even

--- BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Check_Odd_Even.py
+++ BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Check_Odd_Even.py
@@ -1,6 +1,12 @@
-num = int(input("enter your number:"))
-rem = num%2
-if(rem==0):
+# Take an integer input from the user
+num = int(input("Enter your number: "))
+
+# Find the remainder when the number is divided by 2
+rem = num % 2
+
+# If the remainder is 0, then the number is even
+if rem == 0:
     print("even")
+# Otherwise, the number is odd
 else:
     print("odd")
