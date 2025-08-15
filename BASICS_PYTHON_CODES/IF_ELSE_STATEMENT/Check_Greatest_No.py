05f0db8da2160a2d09dfb434ccd29cb91c563048
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:08:40 +0530

    Add comments to check greatest no

--- BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Check_Greatest_No.py
+++ BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Check_Greatest_No.py
@@ -1,12 +1,21 @@
-a = int(input("enter your first num ="))
-b = int(input("enter your sec num ="))
-c = int(input("enter your third num = "))
-d = int(input("enter your fourth num = "))
-if(a>b and a>c and a>d):
-    print("first num is the greatest",a)
-elif(b>c and b>d):
-    print("sec num is the greatest",b)
-elif(c>d):
-    print("third num is the greatest",c)
+# Take four integer inputs from the user
+a = int(input("Enter your first num = "))
+b = int(input("Enter your second num = "))
+c = int(input("Enter your third num = "))
+d = int(input("Enter your fourth num = "))
+
+# Check if 'a' is greater than b, c, and d
+if (a > b and a > c and a > d):
+    print("First num is the greatest", a)
+
+# If the above is false, check if 'b' is greater than c and d
+elif (b > c and b > d):
+    print("Second num is the greatest", b)
+
+# If the above is false, check if 'c' is greater than d
+elif (c > d):
+    print("Third num is the greatest", c)
+
+# If none of the above is true, 'd' must be the greatest
 else:
-    print("fourth num is the greatest",d)
+    print("Fourth num is the greatest", d)
