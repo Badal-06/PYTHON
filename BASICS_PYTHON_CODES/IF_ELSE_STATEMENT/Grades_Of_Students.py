837f10dffcfc6a064acf170658a367767426f094
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:10:56 +0530

    Add comments to grades of students

--- BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Grades_Of_Students.py
+++ BASICS_PYTHON_CODES/IF_ELSE_STATEMENT/Grades_Of_Students.py
@@ -1,12 +1,20 @@
+# Code to determine the grade of a student based on marks
 
-# code for grades of student: -
+# Take integer input for marks from the user
+marks = int(input("marks: "))
 
-marks=int(input("marks:"))
-if(marks>=90):
+# If marks are 90 or above, grade is A
+if marks >= 90:
     print("A")
-elif(marks>=80):
+
+# If marks are between 80 and 89, grade is B
+elif marks >= 80:
     print("B")
-elif(marks>=70):
+
+# If marks are between 70 and 79, grade is C
+elif marks >= 70:
     print("C")
+
+# If marks are below 70, grade is D
 else:
-    print("D")  
+    print("D")
