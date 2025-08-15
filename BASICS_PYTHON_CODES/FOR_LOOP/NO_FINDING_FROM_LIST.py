8b78de64db2b752f6826a524e4f099f3e2ee23bb
badal-06 <badalpanwar91@gmail.com>
2025-08-15 14:57:42 +0530

    Add comments to no finding from list

--- BASICS_PYTHON_CODES/FOR_LOOP/NO_FINDING_FROM_LIST.py
+++ BASICS_PYTHON_CODES/FOR_LOOP/NO_FINDING_FROM_LIST.py
@@ -1,7 +1,18 @@
-nums =[1,4,9,16,25,36,49,64,81,100]
-x=49
-idx=0
+# List of numbers
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Number to search for in the list
+x = 49
+
+# Variable to keep track of the current index in the list
+idx = 0
+
+# Loop through each number in the list
 for no in nums:
-    if(no==x):
-        print("no. found at idx",idx)
-    idx+=1
+    # If the current number matches the search value 'x'
+    if no == x:
+        # Print a message showing the index where the number was found
+        print("no. found at idx", idx)
+    
+    # Move to the next index
+    idx += 1
