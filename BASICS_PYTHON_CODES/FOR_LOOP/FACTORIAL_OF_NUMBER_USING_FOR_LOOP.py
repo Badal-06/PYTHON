a2cb090823cd97fc72dec53300a62689e2eaf0a9
badal-06 <badalpanwar91@gmail.com>
2025-08-15 14:51:37 +0530

    Add comments to factorial of no

--- BASICS_PYTHON_CODES/FOR_LOOP/FACTORIAL_OF_NUMBER_USING_FOR_LOOP.py
+++ BASICS_PYTHON_CODES/FOR_LOOP/FACTORIAL_OF_NUMBER_USING_FOR_LOOP.py
@@ -1,5 +1,13 @@
-n=int(input("enter a number: "))
-fact=1
-for i in range(1,n+1):
-    fact*=i
-print("fact:",fact)
+# Take an integer input from the user
+n = int(input("Enter a number: "))
+
+# Initialize the factorial result variable to 1
+fact = 1
+
+# Loop from 1 to n (inclusive) to calculate factorial
+for i in range(1, n + 1):
+    # Multiply 'fact' by the current number 'i'
+    fact *= i
+
+# Print the final factorial value
+print("fact:", fact)
