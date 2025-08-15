33c64c69836714c9290f084e55b47a7c8e523653
badal-06 <badalpanwar91@gmail.com>
2025-08-15 14:56:20 +0530

    add comments to multiplication table

--- BASICS_PYTHON_CODES/FOR_LOOP/MULTIPLICATION_TABLE_USING_FOR_LOOP.py
+++ BASICS_PYTHON_CODES/FOR_LOOP/MULTIPLICATION_TABLE_USING_FOR_LOOP.py
@@ -1,3 +1,7 @@
-n=int(input("enter no:"))
-for i in range(1,11):
-    print(n*i)
+# Take an integer input from the user (the number for the multiplication table)
+n = int(input("Enter no: "))
+
+# Loop from 1 to 10 to generate the multiplication table
+for i in range(1, 11):
+    # Print in the standard format: "n x i = result"
+    print(f"{n} x {i} = {n * i}")
