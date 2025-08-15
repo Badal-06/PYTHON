415d69bd97ea399c7535d1eb72ebb0d13c1e607a
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:03:23 +0530

    Add comments to capitalize

--- BASICS_PYTHON_CODES/FUNCTION/CAPITALIZE.py
+++ BASICS_PYTHON_CODES/FUNCTION/CAPITALIZE.py
@@ -1,4 +1,11 @@
+# Function to capitalize the first letter of each word in a string
 def capitalize(newstr):
+    # .title() converts the first letter of each word to uppercase 
+    # and the rest of the letters to lowercase
     return newstr.title()
-str=input("Enter the string: ")
+
+# Take input from the user
+str = input("Enter the string: ")
+
+# Call the capitalize() function and print the result
 print(capitalize(str))
