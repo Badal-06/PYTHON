719246453060e0dd28ac0328f46d769055fd06a9
badal-06 <badalpanwar91@gmail.com>
2025-08-15 14:53:22 +0530

    Add comments to Hollow square star pattren

--- BASICS_PYTHON_CODES/FOR_LOOP/HOLLOW_SQUARE_STAR_PATTREN.py
+++ BASICS_PYTHON_CODES/FOR_LOOP/HOLLOW_SQUARE_STAR_PATTREN.py
@@ -1,9 +1,23 @@
-n=int(input("enter a number: "))
-for i in range (1,n+1):
-    if (i==1 or i==n):
-        print("*"*n,end="")
+# Take an integer input from the user (size of the square pattern)
+n = int(input("Enter a number: "))
+
+# Loop from 1 to n (inclusive) to print each row of the square
+for i in range(1, n + 1):
+
+    # If it's the first row or the last row
+    if (i == 1 or i == n):
+        # Print a full row of stars without line break at the end
+        print("*" * n, end="")
+
     else:
-        print("*",end="")
-        print(" "*(n-2),end="")
-        print("*",end="")
+        # Print the first star of the row (left border)
+        print("*", end="")
+
+        # Print spaces in the middle (n-2 spaces for hollow effect)
+        print(" " * (n - 2), end="")
+
+        # Print the last star of the row (right border)
+        print("*", end="")
+
+    # Move to the next line after printing each row
     print("")
