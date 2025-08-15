12c8cc6d8285a12768d320b3bb87aedf904be0aa
badal-06 <badalpanwar91@gmail.com>
2025-08-15 15:17:52 +0530

    Add comments to multiplication table using while loop

--- BASICS_PYTHON_CODES/WHILE_LOOP/Multiplication_Table_Using_While_Loop.py
+++ BASICS_PYTHON_CODES/WHILE_LOOP/Multiplication_Table_Using_While_Loop.py
@@ -1,5 +1,13 @@
-n=int(input("enter no="))
-i=1
-while i<=10:
-    print(n*i)
-    i+=1
+# Take an integer input from the user
+n = int(input("enter no="))
+
+# Initialize counter variable
+i = 1
+
+# Loop from 1 to 10 (inclusive)
+while i <= 10:
+    # Print the product of n and i (multiplication table)
+    print(n * i)
+    
+    # Increment i to move to the next multiplier
+    i += 1
