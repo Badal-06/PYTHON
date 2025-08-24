3e22c0417e4f325a53adec5eac9e4c383f54d4c9
badal-06 <badalpanwar91@gmail.com>
2025-08-24 22:33:34 +0530

    Add Unpickling

---
+++ Pickling_Unpickling/Unpickling.py
@@ -0,0 +1,14 @@
+import pickle, PickClass   # Import pickle module and custom module PickClass
+
+# Open the file 'Student.data' in binary read mode
+with open("Student.data", mode="rb") as f:
+    while True:   # Loop until end of file
+        try:
+            # Load one pickled object from the file
+            obj = pickle.load(f)
+
+            # Call disp() method of the object (defined inside PickClass)
+            obj.disp()
+        except EOFError:   # When end of file is reached
+            print("DONE!!")
+            break          # Exit the loop
