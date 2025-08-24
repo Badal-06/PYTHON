ed4c640be2120c50db5e3668257b3aa0bc3eca46
badal-06 <badalpanwar91@gmail.com>
2025-08-24 22:35:23 +0530

    Add Pickling

---
+++ Pickling_Unpickling/Pickling.py
@@ -0,0 +1,21 @@
+import pickle, PickClass   # Import pickle module and custom module PickClass
+
+# Ask user how many student records they want to enter
+n = int(input("Enter Number of Student: "))
+
+# Open file 'student.data' in binary write mode
+with open("student.data", mode="wb") as f:
+    for i in range(n):   # Loop for each student
+        # Take input for student details
+        name = input("Enter Student Name: ")
+        roll = int(input("Enter Roll: "))
+        address = input("Enter Address: ")
+
+        # Create a Student object (class is defined inside PickClass module)
+        stu1 = PickClass.Student(name, roll, address)
+
+        # Serialize (pickle) the object and write it into the file
+        pickle.dump(stu1, f)
+
+# After loop ends
+print("Pickling Done!!")   # Confirmation message
